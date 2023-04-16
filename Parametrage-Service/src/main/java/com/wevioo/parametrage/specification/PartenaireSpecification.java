a847dff7c8a7a8a5e19e217950c4ea5702a91e60
ahmed chokri <ahmed.chokri@esprit.tn>
2023-04-16 17:08:35 +0100

    Specification Partenaire + Fond

--- Parametrage-Service/src/main/java/com/wevioo/parametrage/specification/PartenaireSpecification.java
+++ Parametrage-Service/src/main/java/com/wevioo/parametrage/specification/PartenaireSpecification.java
@@ -23,19 +23,16 @@ public class PartenaireSpecification {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public static Specification <Partenaire> getPartenairewithSpec(String fond ,String modalite ,String test1 , String test2,String test3) throws ParseException{
-		  System.out.println("test1 " + test1);
+	public static Specification <Partenaire> getPartenairewithSpec(String fond ,String modalite ,String montantMin , String montantMax,String statut) throws ParseException{
 		return ((root,query,criteriaBuilder)->{
 		List<Predicate> predicates = new ArrayList<>();
-		 if (test1 != null && !test1.isEmpty() && !test1.equals("Recherche par montantMin")) {
+		 if (montantMin != null && !montantMin.isEmpty() && !montantMin.equals("Recherche par montantMin")) {
 			
 			 Join<Partenaire, Convention> ConventionJoin = root.join("conventions",JoinType.INNER);
 			  Join<Convention, Modalite> ModaliteJoin = ConventionJoin.join("modalite",JoinType.INNER);
 			  Join<Modalite, Fond> FondJoin = ModaliteJoin.join("fond",JoinType.INNER);
 			  query.distinct(true);
-			
-			  System.out.println("montantMinf " + FondJoin.get("montantMin"));
-	         predicates.add(criteriaBuilder.equal(FondJoin.get("montantMin"), test1));
+	         predicates.add(criteriaBuilder.equal(FondJoin.get("montantMin"), montantMin));
 	     }
 		 if (modalite != null && !modalite.isEmpty() && !modalite.equals("Recherche par modalite")) {
 			
@@ -44,16 +41,16 @@ public class PartenaireSpecification {
 			  query.distinct(true); 
 			  predicates.add(criteriaBuilder.equal(ModaliteJoin.get("nomCompletModalite"), modalite));
 	     }
-		 if (test2 != null && !test2.isEmpty() && !test2.equals("Recherche par montantMax")) {
+		 if (montantMax != null && !montantMax.isEmpty() && !montantMax.equals("Recherche par montantMax")) {
 			  Join<Partenaire, Convention> ConventionJoin = root.join("conventions",JoinType.INNER);
 			  Join<Convention, Modalite> ModaliteJoin = ConventionJoin.join("modalite",JoinType.INNER);
 			  Join<Modalite, Fond> FondJoin = ModaliteJoin.join("fond",JoinType.INNER);
 			  query.distinct(true);
-	         predicates.add(criteriaBuilder.equal(FondJoin.get("montantMax"), test2));
+	         predicates.add(criteriaBuilder.equal(FondJoin.get("montantMax"), montantMax));
 	     }
-	     if (test3 != null && !test3.isEmpty() && !test3.equals("Recherche par statut")) {
+	     if (statut != null && !statut.isEmpty() && !statut.equals("Recherche par statut")) {
 	    	 System.out.println("d5al ");
-	         predicates.add(criteriaBuilder.equal(root.get("statut"), Fondstatut.valueOf(test3.toUpperCase())));
+	         predicates.add(criteriaBuilder.equal(root.get("statut"), Fondstatut.valueOf(statut.toUpperCase())));
 	     }
 	     if (!fond.equals("Recherche par fond") && !fond.isEmpty()) {
 			  Join<Partenaire, Convention> ConventionJoin = root.join("conventions",JoinType.INNER);
