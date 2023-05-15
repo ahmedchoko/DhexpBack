eb085959079b31c175b8648f5edbc6a3478142c8
nourelhoudabenmarzouk <nourelhouda.benmarzouk@outlook.fr>
2023-05-15 15:59:01 +0100

    fixed spelling

--- Demande-Service/src/main/java/com/wevioo/demande/repository/BeneficiaireRepository.java
+++ Demande-Service/src/main/java/com/wevioo/demande/repository/BeneficiaireRepository.java
@@ -1,8 +1,7 @@
 package com.wevioo.demande.repository;
 
 import com.wevioo.demande.entities.Beneficiaire;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface BénéficiaireRepository extends PagingAndSortingRepository<Beneficiaire, Long> {
+public interface BeneficiaireRepository extends PagingAndSortingRepository<Beneficiaire, Long> {
 }
