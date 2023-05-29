0852ffd9fb1feb5e143a1af57c8fdc6a02fd2fbb
ahmed chokri <ahmed.chokri@esprit.tn>
2023-05-29 14:14:13 +0100

    demande + tranche + amortissement - Actif - Passif

--- Demande-Service/src/main/java/com/wevioo/demande/repository/ActifsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wevioo.demande.repository;
-
-import com.wevioo.demande.entities.Actifs;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActifsRepository extends PagingAndSortingRepository<Actifs, Long> {
-}
