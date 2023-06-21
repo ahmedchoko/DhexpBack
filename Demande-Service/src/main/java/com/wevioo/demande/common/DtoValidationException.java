fbb89fd4e89683db6b7f595e980535f220235844
nourelhoudabenmarzouk <nourelhouda.benmarzouk@outlook.fr>
2023-06-21 09:30:09 +0100

    fixed stoploss

---
+++ Demande-Service/src/main/java/com/wevioo/demande/common/DtoValidationException.java
@@ -0,0 +1,5 @@
+package com.wevioo.demande.common;
+
+public class DtoValidationException extends RuntimeException{
+    public DtoValidationException(String errMessage) { super(errMessage);}
+}
---
+++ Parametrage-Service/src/main/java/com/wevioo/parametrage/common/DtoValidationException.java
@@ -0,0 +1,5 @@
+package com.wevioo.parametrage.common;
+
+public class DtoValidationException  extends RuntimeException{
+    public DtoValidationException(String errMessage) { super(errMessage);}
+}
